439abc22949207375dc1423ea69e6be52759ad67
Hugo Amaral <hugoamaralth@gmail.com>
2020-01-08 19:01:41 -0300

    Updates

---
+++ precache-manifest.cac7443a0796c137048193ff7c84fd94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870849a2489c2d93a6be497efdf4352a",
+    "url": "/gold-silver/index.html"
+  },
+  {
+    "revision": "811fe103be8a53ffda7b",
+    "url": "/gold-silver/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9949024e9a9dac1e7768",
+    "url": "/gold-silver/static/js/2.e5dbc529.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gold-silver/static/js/2.e5dbc529.chunk.js.LICENSE"
+  },
+  {
+    "revision": "811fe103be8a53ffda7b",
+    "url": "/gold-silver/static/js/main.ada347c9.chunk.js"
+  },
+  {
+    "revision": "d55fae5df57862f429e1",
+    "url": "/gold-silver/static/js/runtime-main.b4dea8a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gold-silver/static/media/logo.5d5d9eef.svg"
+  }
+]);
