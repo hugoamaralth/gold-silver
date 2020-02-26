d74902250cd13c6d092009ec6d621fca832e8d05
AndreiAmr <andreiamaral74@gmail.com>
2020-02-26 00:28:22 -0300

    page Admin Andrei

---
+++ admin/src/Routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Switch, Route, Redirect} from "react-router-dom";
+import ListaProdutos from "./components/ListaProdutos";
+import NovoProduto from "./components/NovoProduto";
+import Edit from "./components/Edit"
+export default props =>
+   <Switch>
+       <Route path="/produtos/list" component={ListaProdutos}/>
+       <Route path="/produtos/new" component={NovoProduto}/>
+       <Route path="/produtos/edit/:id" component={Edit}/>
+       {/* <Redirect from="*" to="index.html" /> */}
+   </Switch>
---
+++ admin/src/components/serverRequests/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+//o IP estava errado, arrumei e já coloquei o endereço do backend numa varaivel, vai facilitar de trocar em tudo depois
+const SERVER_URL = "http://192.168.1.7:3003/api";
+
+//arrumei o metodo também, siga esse padrão para as demais chamadas;
+export async function listarProdutos(){
+    const ret = await axios.get(`${SERVER_URL}/product`);
+    return ret.data;
+};
+
+export async function produtoPorId(id){
+    const ret = await axios.get(`${SERVER_URL}/product/${id}`);
+    return ret.data;
+};
+
